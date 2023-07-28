9518c3ba5cab462ca424bbedf7cf049c9d314e11
deepansh-sharma <96716734+deepansh-sharma@users.noreply.github.com>
2023-07-29 01:09:26 +0530

    Update myToken.sol

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -1,34 +1,49 @@
+// Functionality
+// Only contract owner should be able to mint tokens
+// Any user can transfer tokens
+// Any user can burn tokens
+// 0x66B0b1d2930059407DcC30F1A2305435fc37315E, 0x6827b8f6cc60497d9bf5210d602C0EcaFDF7C405
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.0;
 
-
-
-contract Token {
-
-    // public variables here
-    string public tokenName = "MINTERS";
-    string public abbrv = "MTR";
-    uint public totalSupply = 0;
+contract MyToken {
+    string public name = "MyToken";
+    string public symbol = "MT";
+    uint256 public totalSupply= 10 * 10**uint256(10);
+    address public owner = msg.sender;
+    mapping(address => uint256) public balanceOf;
     
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
 
-    // mapping variable here
-    mapping (address=> uint) public balances;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
 
-    // mint function
-    function mint(address _address, uint _value) public {
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) public {
+
+        balanceOf[_to] += _value;
         totalSupply += _value;
-        balances[_address] += _value;  
-
+        if(_to != owner){
+            revert("You are not the owner of the contract");
+        } 
     }
-
-
-    // burn function
-    function burn( address _address,uint _value) public {
-        if( balances[_address] >= _value){
-            totalSupply -= _value;
-            balances[_address] -= _value;
-        }
-        
-    }
-
 }
